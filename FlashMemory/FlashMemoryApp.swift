390148c11a2043dcd69f821d6624a0313527688f
TobiasLin6 <tobiaslin6@gmail.com>
2026-01-10 20:48:12 -0500

    Initial Commit

---
+++ FlashMemory/FlashMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashMemoryApp.swift
+//  FlashMemory
+//
+//  Created by Tobias on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
